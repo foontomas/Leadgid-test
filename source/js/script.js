a505a83491c8a2a38256ce30abf1f6ba8c23d9ce
foontomas <tykovkin@gmail.com>
2021-06-08 12:04:56 +0300

    Редактирует скрипт, удаляет мусор из кода

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,15 @@
+//Выбираем элементы из DOM
 adForm = document.querySelector('.form');
+const inputName = document.getElementById('name');
+const inputTel = document.getElementById('phone-number');
+const inputAge = document.getElementById('age');
 
+//Предотвращаем действие по умолчанию при отправке формы и выводим алерт с данными.
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const formData = new FormData(evt.target);
-  //const successMessageTemplate = document.querySelector('#success').content.querySelector('.success'); // содержимое шаблона #success
-  //const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error'); // содержимое шаблона #error
-  //const fields = document.querySelectorAll("input:not([type='button']), select");
-
-    const name = document.getElementById('name');
-    const tel = document.getElementById('tel');
-    const age = document.getElementById('age');
-
-  let text = [];
-  /*fields.forEach(
-    f => { text.push(f.type ==="checkbox" ? f.checked : f.value) }
-    );*/
   alert(`Вы направили серверу следующие данные:
-  имя: ${name};
-  телефон: ${tel};
-  возраст: ${age}.`);
+  имя: ${inputName.value};
+  телефон: ${inputTel.value};
+  возраст: ${inputAge.value}.`);
 });
 
